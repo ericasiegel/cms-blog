3799b7cf5ef434524632e66b1ae15fe7e174477a
Erica Siegel <siegel.erica@gmail.com>
2021-02-13 00:31:15 -0800

    post routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,16 @@
 const User = require('./User'); // import the User model
 const Post = require('./Post'); // import the User model
 
+// associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+
 
 // export models as an object
 module.exports = { User, Post }; 
