52fb2ccdc8e05ef07bb0f23247e722d38f21e7f3
Erica Siegel <siegel.erica@gmail.com>
2021-02-16 23:19:09 -0800

    sessions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -12,6 +12,23 @@ const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'my secret words',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
